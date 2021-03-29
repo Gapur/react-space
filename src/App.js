cf55e6d70071df0848a5cafc19d3e12a89f15390
Gapur Kassym <gapur.kassym@gmail.com>
2021-03-29 10:57:53 +0600

    refactor(src): change box and stars

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { useRef, useEffect, useMemo } from 'react'
 import { Canvas, useFrame } from 'react-three-fiber'
 import create from 'zustand'
+import shallow from 'zustand/shallow'
 import * as THREE from 'three'
 
-const numberOfItems = 200
-const ids = new Array(numberOfItems).fill().map((_, i) => i)
+const BOX_COUNT = 200
+const boxIds = new Array(BOX_COUNT).fill().map((_, i) => i)
 
-const [useStore, api] = create(set => ({
-  boxes: ids,
-  coords: ids.reduce(
+const useStore = create(set => ({
+  boxes: boxIds,
+  coordinates: boxIds.reduce(
     (acc, id) => ({
       ...acc,
       [id]: [Math.random() * Math.PI, Math.random() * Math.PI, Math.random() * Math.PI]
@@ -17,22 +18,22 @@ const [useStore, api] = create(set => ({
   ),
   turn: () => {
     set(state => {
-      const coords = {}
+      const coordinates = {}
       for (let i = 0; i < state.boxes.length; i++) {
         const id = state.boxes[i]
-        const [x, y, z] = state.coords[id]
-        coords[id] = [x + 0.01, y + 0.01, z + 0.01]
+        const [x, y, z] = state.coordinates[id]
+        coordinates[id] = [x + 0.01, y + 0.01, z + 0.01]
       }
-      return { ...state, coords }
+      return { ...state, coordinates }
     })
   }
 }))
 
 function Box({ id }) {
   const mesh = useRef()
-  const coords = useRef([0, 0, 0])
-  useEffect(() => api.subscribe(xyz => (coords.current = xyz), state => state.coords[id]))
-  useFrame(() => mesh.current && mesh.current.rotation.set(...coords.current))
+  const coordinates = useRef([0, 0, 0])
+  useEffect(() => useStore.subscribe(xyz => (coordinates.current = xyz), state => state.coordinates[id]))
+  useFrame(() => mesh.current && mesh.current.rotation.set(...coordinates.current))
   return (
     <mesh ref={mesh}>
       <boxBufferGeometry args={[2, 2, 2]} attach="geometry" />
@@ -50,37 +51,35 @@ function Stars() {
     group.current.rotation.set(r, r, r)
     group.current.scale.set(s, s, s)
   })
-  const [geo, mat, coords] = useMemo(() => {
-    const geo = new THREE.SphereBufferGeometry(1, 10, 10)
-    const mat = new THREE.MeshBasicMaterial({ color: new THREE.Color('lightblue') })
-    const coords = new Array(2000).fill().map(i => [Math.random() * 800 - 400, Math.random() * 800 - 400, Math.random() * 800 - 400])
-    return [geo, mat, coords]
+  const [geometry, material, coordinates] = useMemo(() => {
+    const geometry = new THREE.SphereBufferGeometry(1, 10, 10)
+    const material = new THREE.MeshBasicMaterial({ color: new THREE.Color('lightblue') })
+    const coordinates = new Array(2000).fill().map(i => [Math.random() * 800 - 400, Math.random() * 800 - 400, Math.random() * 800 - 400])
+    return [geometry, material, coordinates]
   }, [])
   return (
     <group ref={group}>
-      {coords.map(([p1, p2, p3], i) => (
-        <mesh key={i} geometry={geo} material={mat} position={[p1, p2, p3]} />
+      {coordinates.map(([p1, p2, p3], idx) => (
+        <mesh key={idx} geometry={geometry} material={material} position={[p1, p2, p3]} />
       ))}
     </group>
   )
 }
 
 function App() {
-  const boxes = useStore(state => state.boxes)
+  const [boxes, turn] = useStore(state => [state.boxes, state.turn], shallow)
 
   useEffect(() => {
     function move() {
-      api.getState().turn()
+      turn()
       requestAnimationFrame(move)
     }
     move()
-  }, [])
+  }, [turn])
 
   return (
     <Canvas>
-      {boxes.map(id => (
-        <Box key={id} id={id} />
-      ))}
+      {boxes.map(id => <Box key={id} id={id} />)}
       <Stars />
     </Canvas>
   )
